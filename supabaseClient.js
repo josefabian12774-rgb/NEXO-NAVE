63fc8561e5a6bd5c1444c2193621d1af0efd7b51
josefabian12774-rgb <josefabian12774@gmail.com>
2026-04-25 20:47:02 -0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,2 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+export const supabase = createClient('https://manaejtcurxbcsozqcsh.supabase.co', 'sb_publishable_403c94f06883f3e1a0525946398f61559815')
