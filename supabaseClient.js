e835984ce820ce113d04f96c4fd4428f6ae8c7f8
josefabian12774-rgb <josefabian12774@gmail.com>
2026-04-25 21:27:08 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,2 +1,4 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-export const supabase = createClient('https://manaejtcurxbcsozqcsh.supabase.co', 'sb_publishable_403c94f06883f3e1a0525946398f61559815')
+const supabaseUrl = 'https://manaejtcurxbcsozqcsh.supabase.co'
+const supabaseKey = 'sb_publishable_403c94f06883f3e1a0525946398f61559815'
+export const supabase = createClient(supabaseUrl, supabaseKey)
